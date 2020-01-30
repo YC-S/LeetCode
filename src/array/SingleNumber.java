906ab125c8695930bd64cb64e7d8428a74f8afc6
yuanchen <yuanchenshi@gmail.com>
2020-01-30 17:44:02 -0600

    Single Number

--- src/array/SingleNumber.java
+++ src/array/SingleNumber.java
@@ -6,7 +6,16 @@ package array;
 // Input: [4,1,2,1,2]
 // Output: 4
 public class SingleNumber {
-  public int singleNumber(int[] nums) {
-    return 0;
+  public static int singleNumber(int[] nums) {
+    int res = 0;
+    for (int num : nums) {
+      res ^= num;
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    int[] input = new int[]{4, 1, 2, 1, 2};
+    System.out.println(singleNumber(input));
   }
 }
