8f1e764de84442ca7d54a21aa54ce7b568f7ee9d
yuanchen <yuanchenshi@gmail.com>
2020-01-30 17:38:09 -0600

    Add Files

---
+++ src/array/SingleNumber.java
@@ -0,0 +1,12 @@
+package array;
+
+// Input: [2,2,1]
+// Output: 1
+
+// Input: [4,1,2,1,2]
+// Output: 4
+public class SingleNumber {
+  public int singleNumber(int[] nums) {
+    return 0;
+  }
+}
