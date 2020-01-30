e833bd88e19247b4e9d77641bdea2ddac75171bf
yuanchen <yuanchenshi@gmail.com>
2020-01-30 17:06:37 -0600

    Contains Duplicates

---
+++ src/array/ContainsDuplicates.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Input: [1,2,3,1]
+// Output: true
+public class ContainsDuplicates {
+  public static boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      if (set.contains(num)) {
+        return true;
+      } else {
+        set.add(num);
+      }
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1,1,2,3};
+    System.out.println(containsDuplicate(nums));
+  }
+}
