589fb3eb714bf1e7d4b8fdf829871304b21fd290
yuanchen <yuanchenshi@gmail.com>
2020-01-31 20:42:33 -0600

    Remove Nth Node From End

---
+++ src/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class ReverseLinkedList {
+
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
+  }
+}
