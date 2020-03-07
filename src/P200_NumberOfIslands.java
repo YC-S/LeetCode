f68051916831e59c4eda9edbd0b28c8be5324560
yuanchen <yuanchenshi@gmail.com>
2020-03-07 10:18:31 -0600

    P200_NumberOfIslands

---
+++ src/P200_NumberOfIslands.java
@@ -0,0 +1,35 @@
+public class P200_NumberOfIslands {
+	public static int numIsLands(char[][] grid) {
+		if (grid == null || grid.length == 0) {
+			return 0;
+		}
+		int num = 0;
+		int nr = grid.length;
+		int nc = grid[0].length;
+		for (int i = 0; i < nr; i++) {
+			for (int j = 0; j < nc; j++) {
+				if (grid[i][j] == '1') {
+					num++;
+					dfs(grid, i, j);
+				}
+			}
+		}
+		return num;
+	}
+
+	private static void dfs(char[][] grid, int i, int j) {
+		if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] == '0') {
+			return;
+		}
+		grid[i][j] = '0';
+		dfs(grid, i + 1, j);
+		dfs(grid, i, j + 1);
+		dfs(grid, i - 1, j);
+		dfs(grid, i, j - 1);
+	}
+
+	public static void main(String[] args) {
+		char[][] array = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+		System.out.println(numIsLands(array));
+	}
+}
