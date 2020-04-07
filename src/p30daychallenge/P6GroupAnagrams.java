f652221c9f7f3460fe5b3419870b08257888c5bf
yuanchen <yuanchenshi@gmail.com>
2020-04-06 22:00:25 -0400

    6 Group Anagrams

---
+++ src/p30daychallenge/P6GroupAnagrams.java
@@ -0,0 +1,31 @@
+package p30daychallenge;
+
+import java.util.*;
+
+/**
+ * @author shiyuanchen
+ */
+public class P6GroupAnagrams {
+	public static List<List<String>> groupAnagrams(String[] strs) {
+		if (strs == null || strs.length == 0) {
+			return new ArrayList<>();
+		} else {
+			Map<String, List<String>> map = new HashMap<>(10);
+			for (String s : strs) {
+				char[] ca = s.toCharArray();
+				Arrays.sort(ca);
+				String keyStr = String.valueOf(ca);
+				if (!map.containsKey(keyStr)) {
+					map.put(keyStr, new ArrayList<>());
+				}
+				map.get(keyStr).add(s);
+			}
+			return new ArrayList<>(map.values());
+		}
+	}
+
+	public static void main(String[] args) {
+		String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+		System.out.println(groupAnagrams(input));
+	}
+}
