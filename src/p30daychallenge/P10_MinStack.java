ae5137253236b7d2dd8275a16e0b7eec0be16277
yuanchen <yuanchenshi@gmail.com>
2020-04-10 23:28:24 -0400

    10 Min Stack

---
+++ src/p30daychallenge/P10_MinStack.java
@@ -0,0 +1,48 @@
+package p30daychallenge;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author shiyuanchen
+ */
+public class P10_MinStack {
+	private final Deque<Integer> stack;
+	private final Deque<Integer> minStack;
+
+	/**
+	 * initialize your data structure here.
+	 */
+	public P10_MinStack() {
+		stack = new ArrayDeque<>();
+		minStack = new ArrayDeque<>();
+	}
+
+	public void push(int x) {
+		stack.offerFirst(x);
+		if (minStack.isEmpty() || x <= minStack.peekFirst()) {
+			minStack.offerFirst(x);
+		}
+	}
+
+	public void pop() {
+		if (stack.isEmpty()) {
+			return;
+		}
+		Integer res = stack.pollFirst();
+		if (!minStack.isEmpty() && minStack.peekFirst().equals(res)) {
+			minStack.pollFirst();
+		}
+	}
+
+	public int top() {
+		if (stack.isEmpty()) {
+			return -1;
+		}
+		return stack.peekFirst();
+	}
+
+	public int getMin() {
+		return minStack.isEmpty() ? -1 : minStack.peekFirst();
+	}
+}
