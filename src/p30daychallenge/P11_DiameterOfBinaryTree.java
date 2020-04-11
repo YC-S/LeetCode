52620ac0e3b2a404efb3fb2a7ee6378c80f71041
yuanchen <yuanchenshi@gmail.com>
2020-04-11 17:17:24 -0400

    11 Diameter of Binary Tree

---
+++ src/p30daychallenge/P11_DiameterOfBinaryTree.java
@@ -0,0 +1,35 @@
+package p30daychallenge;
+
+/**
+ * @author shiyuanchen
+ */
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class P11_DiameterOfBinaryTree {
+	int ans;
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		ans = 1;
+		depth(root);
+		return ans - 1;
+	}
+
+	public int depth(TreeNode node) {
+		if (node == null) {
+			return 0;
+		}
+		int l = depth(node.left);
+		int r = depth(node.right);
+		ans = Math.max(ans, l + r + 1);
+		return Math.max(l, r) + 1;
+	}
+}
