c07ab4e6611d5d69a8684603fbbed92fea20c786
yuanchen <yuanchenshi@gmail.com>
2020-04-17 19:59:11 -0400

    16 Check Valid String

---
+++ src/p30daychallenge/P16CheckvalidString.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package p30daychallenge;
+
+public class P16CheckvalidString {
+
+    public static boolean checkValidString(String s) {
+        int lo = 0, hi = 0;
+        for (char c : s.toCharArray()) {
+            lo += c == '(' ? 1 : -1;
+            hi += c != ')' ? 1 : -1;
+            if (hi < 0) {
+                break;
+            }
+            lo = Math.max(lo, 0);
+        }
+        return lo == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkValidString("()"));
+        System.out.println(checkValidString("(*)"));
+        System.out.println(checkValidString("(*))"));
+    }
+}
