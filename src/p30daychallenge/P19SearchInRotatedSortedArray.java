b7631accaea1016f54182717959c283de8c60b60
yuanchen <yuanchenshi@gmail.com>
2020-04-19 11:52:54 -0400

    19 Search In Rotated Sorted Array

---
+++ src/p30daychallenge/P19SearchInRotatedSortedArray.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package p30daychallenge;
+
+/**
+ * @author shiyuanchen
+ */
+public class P19SearchInRotatedSortedArray {
+
+    public static int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 3};
+        System.out.println(search(nums, 3));
+    }
+
+}
