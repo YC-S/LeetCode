263ee6651545d33cf5f4f828d1f6eafff8b6f1e5
yuanchen <yuanchenshi@gmail.com>
2020-04-23 22:55:36 -0400

    23 bitwise and of number range

---
+++ src/p30daychallenge/P23BitwiseAndOfNumbersRange.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package p30daychallenge;
+
+/**
+ * @author shiyuanchen
+ */
+public class P23BitwiseAndOfNumbersRange {
+
+    public static int rangeBitwiseAnd(int m, int n) {
+        if (m == 0) {
+            return 0;
+        }
+        int moveFactor = 1;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            moveFactor <<= 1;
+        }
+        return m * moveFactor;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rangeBitwiseAnd(5, 7));
+    }
+}
