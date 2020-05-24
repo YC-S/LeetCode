6288d6cbe5345e15299aacfb84440a70668e05ee
yuanchen <yuanchenshi@gmail.com>
2020-05-24 04:26:24 -0400

    P24 Construct BST From Preorder Traversal

---
+++ src/may_challenge/P24ConstructBSTFromPreorderTraversal.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package may_challenge;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import utility.TreeNode;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/05/24
+ */
+public class P24ConstructBSTFromPreorderTraversal {
+
+  public TreeNode bstFromPreorder(int[] preorder) {
+    int n = preorder.length;
+    if (n == 0) {
+      return null;
+    }
+
+    TreeNode root = new TreeNode(preorder[0]);
+    Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+    deque.push(root);
+
+    for (int i = 1; i < n; i++) {
+      // take the last element of the deque as a parent
+      // and create a child from the next preorder element
+      TreeNode node = deque.peek();
+      TreeNode child = new TreeNode(preorder[i]);
+      // adjust the parent
+      while (!deque.isEmpty() && deque.peek().val < child.val) {
+        node = deque.pop();
+      }
+
+      // follow BST logic to create a parent-child link
+      if (node.val < child.val) {
+        node.right = child;
+      } else {
+        node.left = child;
+      }
+      // add the child into deque
+      deque.push(child);
+    }
+    return root;
+  }
+}
