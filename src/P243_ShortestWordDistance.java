df71f37644dcdc7b91268ee26ab67dc040f0e2c6
yuanchen <yuanchenshi@gmail.com>
2020-06-01 11:32:25 -0400

    P243 ShortestDistance

---
+++ src/P243_ShortestWordDistance.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/06/01
+ */
+public class P243_ShortestWordDistance {
+
+  public int shortestDistance(String[] words, String word1, String word2) {
+    int p1 = -1, p2 = -1, min = Integer.MAX_VALUE;
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].equals(word1)) {
+        p1 = i;
+      }
+      if (words[i].equals(word2)) {
+        p2 = i;
+      }
+      if (p1 != -1 && p2 != -1) {
+        min = Math.min(min, Math.abs(p1 - p2));
+      }
+    }
+    return min;
+  }
+}
