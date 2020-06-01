a002495f9d44f731832b8afcd8133f6f769112bc
yuanchen <yuanchenshi@gmail.com>
2020-06-01 11:08:42 -0400

    P6 Zig Zag Conversion

--- src/P5_LongestPalindromicSubstring.java
+++ src/P5_LongestPalindromicSubstring.java
@@ -2,8 +2,6 @@
  * Copyright (c) 2020. Yuanchen
  */
 
-package utility;
-
 /**
  * @author shiyuanchen
  * @project LeetCode
---
+++ src/P6_ZigZagConversion.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/06/01
+ */
+public class P6_ZigZagConversion {
+
+  public String convert(String s, int numRows) {
+    if (numRows == 1) {
+      return s;
+    }
+
+    StringBuilder ret = new StringBuilder();
+    int n = s.length();
+    int cycleLen = 2 * numRows - 2;
+
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j + i < n; j += cycleLen) {
+        ret.append(s.charAt(j + i));
+        if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+          ret.append(s.charAt(j + cycleLen - i));
+        }
+      }
+    }
+    return ret.toString();
+  }
+}
