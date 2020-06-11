34e4096bf05f2e5887ce27ae8a8ece794cf308fb
yuanchen <yuanchenshi@gmail.com>
2020-06-10 23:34:38 -0400

    June 10 Search Insert Position

---
+++ src/june_challenge/P10SearchInsertPosition.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package june_challenge;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/06/10
+ */
+public class P10SearchInsertPosition {
+
+  public int searchInsert(int[] A, int target) {
+    if (A == null || A.length == 0) {
+      return 0;
+    }
+    int start = 0, end = A.length - 1;
+
+    while (start + 1 < end) {
+      int mid = start + (end - start) / 2;
+      if (A[mid] == target) {
+        return mid;
+      } else if (A[mid] < target) {
+        start = mid;
+      } else {
+        end = mid;
+      }
+    }
+
+    if (A[start] >= target) {
+      return start;
+    } else if (A[end] >= target) {
+      return end;
+    } else {
+      return end + 1;
+    }
+  }
+}
