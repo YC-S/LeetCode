6fe73aac2105cc856f8b13b7e86cb630ac81b6ed
yuanchen <yuanchenshi@gmail.com>
2020-06-20 17:08:34 -0400

    June 20 Permutation Sequence

---
+++ src/june_challenge/P20PermutationSequence.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package june_challenge;
+
+import java.util.ArrayList;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/06/20
+ */
+public class P20PermutationSequence {
+
+  public String getPermutation(int n, int k) {
+    StringBuilder sb = new StringBuilder();
+    ArrayList<Integer> num = new ArrayList<Integer>();
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+      fact *= i;
+      num.add(i);
+    }
+    for (int i = 0, l = k - 1; i < n; i++) {
+      fact /= (n - i);
+      int index = (l / fact);
+      sb.append(num.remove(index));
+      l -= index * fact;
+    }
+    return sb.toString();
+  }
+}
