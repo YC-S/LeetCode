24c7a86dc85efbdc251e04422f05822251c9fd34
yuanchen <yuanchenshi@gmail.com>
2020-06-26 01:34:01 -0400

    June 25 Find Duplicate

---
+++ src/june_challenge/P25FindDuplicate.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package june_challenge;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/06/26
+ */
+public class P25FindDuplicate {
+
+  public int findDuplicate(int[] nums) {
+    if (nums.length > 1) {
+      int slow = nums[0];
+      int fast = nums[nums[0]];
+      while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+      }
+
+      fast = 0;
+      while (fast != slow) {
+        fast = nums[fast];
+        slow = nums[slow];
+      }
+      return slow;
+    }
+    return -1;
+  }
+}
