597a9c873d235c71a81cd781cf776aee8f3746e1
yuanchen <yuanchenshi@gmail.com>
2020-07-08 15:36:40 -0400

    P581 Shortest Unsorted Continuous Subarray

---
+++ src/top100/P581ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package top100;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/07/08
+ */
+public class P581ShortestUnsortedContinuousSubarray {
+
+  public int findUnsortedSubarray(int[] nums) {
+    int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    boolean flag = false;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] < nums[i - 1]) {
+        flag = true;
+      }
+      if (flag) {
+        min = Math.min(min, nums[i]);
+      }
+    }
+    flag = false;
+    for (int i = nums.length - 2; i >= 0; i--) {
+      if (nums[i] > nums[i + 1]) {
+        flag = true;
+      }
+      if (flag) {
+        max = Math.max(max, nums[i]);
+      }
+    }
+    int l, r;
+    for (l = 0; l < nums.length; l++) {
+      if (min < nums[l]) {
+        break;
+      }
+    }
+    for (r = nums.length - 1; r >= 0; r--) {
+      if (max > nums[r]) {
+        break;
+      }
+    }
+    return r - l < 0 ? 0 : r - l + 1;
+  }
+}
