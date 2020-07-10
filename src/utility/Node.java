9581ca00900c7eca503ba8ea54ffb0ace559367e
yuanchen <yuanchenshi@gmail.com>
2020-07-10 18:03:17 -0400

    P10 Flatten A multilevel Doubly LinkedList

---
+++ src/utility/Node.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package utility;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/07/10
+ */
+public class Node {
+
+  public int val;
+  public Node prev;
+  public Node next;
+  public Node child;
+
+  public Node(int i, Object o, Node head, Object o1) {
+  }
+}
