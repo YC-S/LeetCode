0b55fd207b4670249cd5cee73d274425f0c07a0d
YC-S <yuanchenshi@gmail.com>
2020-09-05 08:23:10 -0400

    September 4th Recursion

---
+++ src/recursion_101/ReverseString.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package recursion_101;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/09/02
+ */
+public class ReverseString {
+
+  public static void reverseString(char[] s) {
+    helper(s, 0, s.length - 1);
+  }
+
+  private static void helper(char[] s, int left, int right) {
+    if (left >= right) {
+      return;
+    }
+    char tmp = s[left];
+    s[left++] = s[right];
+    s[right--] = tmp;
+    helper(s, left, right);
+  }
+
+  public static void main(String[] args) {
+    char[] input = {'h', 'e', 'l', 'l', 'o'};
+    reverseString(input);
+    System.out.println(input);
+  }
+}
