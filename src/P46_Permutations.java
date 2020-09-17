eaadd62481d8d6ddf6f0967dca1fc857ee8978ef
YC-S <yuanchenshi@gmail.com>
2020-09-17 07:36:30 -0400

    September 17th Permutations

---
+++ src/P46_Permutations.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/09/17
+ */
+public class P46_Permutations {
+
+  public static void main(String[] args) {
+
+  }
+
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (nums == null) {
+      return res;
+    }
+    helper(nums, 0, res);
+    return res;
+  }
+
+  private void helper(int[] array, int index, List<List<Integer>> res) {
+    if (index == array.length) {
+      List<Integer> tmp = new ArrayList<>();
+      for (int num : array) {
+        tmp.add(num);
+      }
+      res.add(tmp);
+      return;
+    }
+    for (int i = index; i < array.length; ++i) {
+      swap(array, i, index);
+      helper(array, index + 1, res);
+      swap(array, i, index);
+    }
+  }
+
+  private void swap(int[] array, int a, int b) {
+    int tmp = array[a];
+    array[a] = array[b];
+    array[b] = tmp;
+  }
+}
---
+++ src/java_basics/JavaBasics.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package java_basics;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/09/11
+ */
+public class JavaBasics {
+
+  public static void main(String[] args) {
+    // Java 中的 Math. round(-1. 5) 等于多少？
+    System.out.println(Math.round(-1.5));
+  }
+
+
+}
