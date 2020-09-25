b68f886488e2244f0a19ed1a195d006b3a89b304
YC-S <yuanchenshi@gmail.com>
2020-09-24 22:15:21 -0400

    September 24th Tree Questions

---
+++ src/P191_NumberOf1Bits.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/09/23
+ */
+public class P191_NumberOf1Bits {
+
+  public static int hammingWeight(int n) {
+    int res = 0;
+    while (n != 0) {
+      res += n & 1;
+      n >>>= 1;
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(hammingWeight(0b00000000000000000000000000001011));
+  }
+}
