0b0ecf654c3f6474e59750c0a58b3ddfabb52c42
YC-S <yuanchenshi@gmail.com>
2020-11-17 21:21:53 -0500

    OA

---
+++ src/amazon_oa/ella/Q1.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package amazon_oa.ella;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/11/16
+ */
+public class Q1 {
+
+    static String LexicographicalMaxString(String s) {
+        // loop to find the max leicographic
+        // substring in the substring array
+        String max = "";
+        for (int i = 0; i < s.length(); ++i) {
+            if (max.compareTo(s.substring(i)) <= 0) {
+                max = s.substring(i);
+            }
+        }
+
+        return max;
+    }
+
+}
