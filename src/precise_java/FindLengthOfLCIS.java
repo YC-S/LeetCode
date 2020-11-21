96f4807eed4c691df1d84356faea356d93df34cb
YC-S <yuanchenshi@gmail.com>
2020-11-21 14:17:22 -0500

    google phone interview

---
+++ src/precise_java/FindLengthOfLCIS.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020. Yuanchen
+ */
+
+package precise_java;
+
+/**
+ * @author shiyuanchen
+ * @project LeetCode
+ * @since 2020/11/21
+ */
+public class FindLengthOfLCIS {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 4, 7};
+        int[] nums1 = {2, 2, 2, 2, 2};
+        System.out.println(new FindLengthOfLCIS().findLengthOfLCIS(nums));
+        System.out.println(new FindLengthOfLCIS().findLengthOfLCIS(nums1));
+    }
+
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 1;
+        int max = 1;
+        int cur = 1;
+        while (right < nums.length) {
+            if (nums[right] > nums[right - 1]) {
+                cur++;
+                max = Math.max(cur, max);
+                right++;
+            } else {
+                left = right;
+                right++;
+                cur = 1;
+            }
+        }
+        return max;
+    }
+
+}
