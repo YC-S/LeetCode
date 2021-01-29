fb9d06086f1f4f19c9b4848480b2bf72770a61ed
YC-S <yuanchenshi@gmail.com>
2021-01-29 11:53:17 -0500

    add files

---
+++ src/explore/month_challenge/_2021_january/week5/Day1NumberOfCornerRectangles.java
@@ -0,0 +1,17 @@
+package explore.month_challenge._2021_january.week5;
+
+public class Day1NumberOfCornerRectangles {
+    public int countCornerRectangles(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int j = i + 1; j < grid.length; j++) {
+                int counter = 0;
+                for (int k = 0; k < grid[0].length; k++) {
+                    if (grid[i][k] == 1 && grid[j][k] == 1) counter++;
+                }
+                if (counter > 0) ans += counter * (counter - 1) / 2;
+            }
+        }
+        return ans;
+    }
+}
