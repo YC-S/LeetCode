48b0ca03da3766084556837f198dae2f65a404dd
YC-S <yuanchenshi@gmail.com>
2021-02-04 22:26:54 -0500

    add files

---
+++ src/explore/month_challenge/_2021_february/week1/Day4LongestHarmoniousSubsequence.java
@@ -0,0 +1,20 @@
+package explore.month_challenge._2021_february.week1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day4LongestHarmoniousSubsequence {
+    public int findLHS(int[] nums) {
+        Map<Long, Integer> map = new HashMap<>();
+        for (long num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int result = 0;
+        for (long key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                result = Math.max(result, map.get(key + 1) + map.get(key));
+            }
+        }
+        return result;
+    }
+}
