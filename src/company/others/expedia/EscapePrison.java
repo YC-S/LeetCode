51273030bab1167291521eebaf80f2ee0d8f305b
YC-S <yuanchenshi@gmail.com>
2021-04-14 17:10:37 -0400

    Expedia OA

--- src/company/others/expedia/EscapePrison.java
+++ src/company/others/expedia/EscapePrison.java
@@ -1,65 +1,25 @@
 package company.others.expedia;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class EscapePrison {
     static long prison(int n, int m, List<Integer> h, List<Integer> v) {
-        ArrayList<ArrayList<Long>> prison = new ArrayList<ArrayList<Long>>();
-        for (int r = 0; r <= n; r++) {
-            ArrayList<Long> temp = new ArrayList<Long>();
-            for (int c = 0; c <= m; c++) {
-                temp.add((long) 1);
-            }
-            prison.add(temp);
+        boolean[] xs = new boolean[n + 1];
+        boolean[] ys = new boolean[m + 1];
+        for (Integer i : h) xs[i] = true;
+        for (Integer i : v) ys[i] = true;
+        int xm = 0, ym = 0;
+        for (int i = 1, j = 0; i <= n; i++) {
+            if (!xs[i]) j = 0;
+            else xm = Math.max(xm, ++j);
         }
-
-        List<Integer> x = h;
-        int xnum = x.size();
-        Collections.sort(x);
-        List<Integer> y = v;
-        int ynum = y.size();
-        Collections.sort(y);
-
-        //removing bar i means that list at i-1 and at i
-        for (int a = xnum - 1; a >= 0; a--) {
-            int i = x.get(a);
-            for (int cell = 0; cell < prison.get(i).size(); cell++) {
-                prison.get(i).set(cell, prison.get(i).get(cell) + prison.get(i - 1).get(cell));
-            }
-            prison.remove(i - 1);
+        for (int i = 1, j = 0; i <= m; i++) {
+            if (!ys[i]) j = 0;
+            else ym = Math.max(ym, ++j);
         }
-
-
-        ArrayList<ArrayList<Long>> newprison = new ArrayList<ArrayList<Long>>();
-
-        for (int col = 0; col < prison.get(0).size(); col++) {
-            ArrayList<Long> temp = new ArrayList<Long>();
-            for (int row = 0; row < prison.size(); row++) {
-                temp.add(prison.get(row).get(col));
-            }
-            newprison.add(temp);
-        }
-
-        for (int b = ynum - 1; b >= 0; b--) {
-            int i = y.get(b);
-            for (int cell = 0; cell < newprison.get(i).size(); cell++) {
-                newprison.get(i).set(cell, newprison.get(i).get(cell) + newprison.get(i - 1).get(cell));
-            }
-            newprison.remove(i - 1);
-        }
-
-        long max = 1;
-        for (ArrayList<Long> arr : newprison) {
-            for (long num : arr) {
-                if (num > max)
-                    max = num;
-            }
-        }
-        return max;
+        return (long) (xm + 1) * (ym + 1);
     }
 
     public static void main(String[] args) throws IOException {
--- src/company/others/expedia/FindTheFactor.java
+++ src/company/others/expedia/FindTheFactor.java
@@ -1,25 +1,22 @@
 package company.others.expedia;
 
 public class FindTheFactor {
-    public static int nthUglyNumber(int n, int p) {
-        int[] ugly = new int[n];
-        ugly[0] = 1;
-        int index2 = 0, index3 = 0, index5 = 0;
-        int factor2 = 2, factor3 = 3, factor5 = 5;
-        for (int i = 1; i < n; i++) {
-            int min = Math.min(Math.min(factor2, factor3), factor5);
-            ugly[i] = min;
-            if (factor2 == min)
-                factor2 = 2 * ugly[++index2];
-            if (factor3 == min)
-                factor3 = 3 * ugly[++index3];
-            if (factor5 == min)
-                factor5 = 5 * ugly[++index5];
+    public static long nthUglyNumber(long n, long p) {
+        long i = 1;
+        for (; i * i <= n; i++)
+            if (n % i == 0 && --p == 0)
+                return i;
+        i = i - 1;
+        if (i * i == n)
+            i = i - 1;
+        for (; i >= 1; i--) {
+            if (n % i == 0 && --p == 0)
+                return n / i;
         }
-        return p <= n - 1 ? ugly[p - 1] : -1;
+        return 0;
     }
 
     public static void main(String[] args) {
-        System.out.println(nthUglyNumber(10, 5));
+        System.out.println(nthUglyNumber(10, 3));
     }
 }
