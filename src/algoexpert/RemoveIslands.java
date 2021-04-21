96d717ac7ec80cc8b8d9369fec2aa0bd40029650
YC-S <yuanchenshi@gmail.com>
2021-04-21 16:35:25 -0400

    Remove Islands

--- src/algoexpert/RemoveIslands.java
+++ src/algoexpert/RemoveIslands.java
@@ -1,34 +1,86 @@
 package algoexpert;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class RemoveIslands {
-    public static int[][] removeIslands(int[][] matrix) {
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        boolean[][] visited = new boolean[rows][columns];
-        int[][] copyGrid = new int[rows][columns];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                copyGrid[i][j] = matrix[i][j];
+    public int[][] removeIslands(int[][] matrix) {
+        boolean[][] onesConnectedToBorder = new boolean[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            onesConnectedToBorder[i][matrix[0].length - 1] = false;
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                boolean rowIsBorder = row == 0 || row == matrix.length - 1;
+                boolean colIsBorder = col == 0 || col == matrix[0].length - 1;
+                boolean isBorder = rowIsBorder || colIsBorder;
+
+                if (!isBorder) {
+                    continue;
+                }
+
+                if (matrix[row][col] != 1) {
+                    continue;
+                }
+                findOnesConnectedToBorder(matrix, row, col, onesConnectedToBorder);
             }
         }
-        DFS(matrix, copyGrid, visited, 0, 0);
-        return copyGrid;
+        for (int row = 1; row < matrix.length - 1; row++) {
+            for (int col = 1; col < matrix[0].length - 1; col++) {
+                if (onesConnectedToBorder[row][col]) {
+                    continue;
+                }
+                matrix[row][col] = 0;
+            }
+        }
+        return matrix;
     }
 
-    private static void DFS(int[][] grid, int[][] copyGrid, boolean[][] visited, int i, int j) {
-        int rows = grid.length;
-        int columns = grid[0].length;
-        if (i < 0 || i >= rows || j < 0 || j >= columns || visited[i][j]) {
-            return;
+    public void findOnesConnectedToBorder(int[][] matrix, int startRow, int startCol, boolean[][] onesConnectedToBorder) {
+        Stack<int[]> stack = new Stack<int[]>();
+        stack.push(new int[]{startRow, startCol});
+        while (stack.size() > 0) {
+            int[] currentPosition = stack.pop();
+            int currentRow = currentPosition[0];
+            int currentCol = currentPosition[1];
+
+            boolean alreadyVisited = onesConnectedToBorder[currentRow][currentCol];
+            if (alreadyVisited) {
+                continue;
+            }
+            onesConnectedToBorder[currentRow][currentCol] = true;
+            int[][] neighbors = getNeighbors(matrix, currentRow, currentCol);
+            for (int[] neighbor : neighbors) {
+                int row = neighbor[0];
+                int col = neighbor[1];
+                if (matrix[row][col] != 1) {
+                    continue;
+                }
+                stack.push(neighbor);
+            }
         }
-        visited[i][j] = true;
-        if (i > 0 && grid[i - 1][j] == 1 && i < rows - 1 && grid[i + 1][j] == 1 && j > 0 && grid[i][j - 1] == 1
-                && j < columns - 1 && grid[i][j + 1] == 1) {
-            copyGrid[i][j] = 0;
+    }
+
+    public int[][] getNeighbors(int[][] matrix, int row, int col) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        ArrayList<int[]> temp = new ArrayList<>();
+        if (row - 1 >= 0) {
+            temp.add(new int[]{row - 1, col});
         }
-        DFS(grid, copyGrid, visited, i + 1, j);
-        DFS(grid, copyGrid, visited, i - 1, j);
-        DFS(grid, copyGrid, visited, i, j + 1);
-        DFS(grid, copyGrid, visited, i, j - 1);
+        if (row + 1 < numRows) {
+            temp.add(new int[]{row + 1, col});
+        }
+        if (col - 1 >= 0) {
+            temp.add(new int[]{row, col - 1});
+        }
+        if (col + 1 < numCols) {
+            temp.add(new int[]{row, col + 1});
+        }
+        int[][] neighbors = new int[temp.size()][2];
+        for (int i = 0; i < temp.size(); i++) {
+            neighbors[i] = temp.get(i);
+        }
+        return neighbors;
     }
 }
