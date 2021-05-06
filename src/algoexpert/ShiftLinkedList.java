479e179acd92f53d0b528f90db87366509f57dd1
YC-S <yuanchenshi@gmail.com>
2021-05-06 19:17:17 -0400

    shift linked list

---
+++ src/algoexpert/ShiftLinkedList.java
@@ -0,0 +1,35 @@
+package algoexpert;
+
+public class ShiftLinkedList {
+  public static LinkedList shiftLinkedList(LinkedList head, int k) {
+    // Write your code here.
+    int listLength = 1;
+    LinkedList listTail = head;
+    while (listTail.next != null) {
+      listTail = listTail.next;
+      listLength++;
+    }
+    int offset = Math.abs(k) % listLength;
+    if (offset == 0) return head;
+    int newTailPosition = k > 0 ? listLength - offset : offset;
+    LinkedList newTail = head;
+    for (int i = 1; i < newTailPosition; i++) {
+      newTail = newTail.next;
+    }
+
+    LinkedList newHead = newTail.next;
+    newTail.next = null;
+    listTail.next = head;
+    return newHead;
+  }
+
+  static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      next = null;
+    }
+  }
+}
