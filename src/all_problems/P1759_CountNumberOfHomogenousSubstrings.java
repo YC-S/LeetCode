cbc0fd31e99d0e45bc2fd04fd31e53c5db708e3f
YC-S <yuanchenshi@gmail.com>
2021-05-26 18:52:09 -0400

    P1759_CountNumberOfHomogenousSubstrings

---
+++ src/all_problems/P1759_CountNumberOfHomogenousSubstrings.java
@@ -0,0 +1,13 @@
+package all_problems;
+
+public class P1759_CountNumberOfHomogenousSubstrings {
+  public int countHomogenous(String s) {
+    int res = 0, cur = 0, count = 0, mod = 1_000_000_007;
+    for (int i = 0; i < s.length(); i++) {
+      count = s.charAt(i) == cur ? count + 1 : 1;
+      cur = s.charAt(i);
+      res = (res + count) % mod;
+    }
+    return res;
+  }
+}
