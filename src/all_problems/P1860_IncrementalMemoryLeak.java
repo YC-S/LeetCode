30eefdd3a7cf7bad6a75f83d9e3f98df71a0a53f
YC-S <yuanchenshi@gmail.com>
2021-05-27 11:10:44 -0400

    P1860_IncrementalMemoryLeak

---
+++ src/all_problems/P1860_IncrementalMemoryLeak.java
@@ -0,0 +1,13 @@
+package all_problems;
+
+public class P1860_IncrementalMemoryLeak {
+  public int[] memLeak(int memory1, int memory2) {
+    int i = 1;
+    while (Math.max(memory1, memory2) >= i) {
+      if (memory1 >= memory2) memory1 -= i;
+      else memory2 -= i;
+      i++;
+    }
+    return new int[] {i, memory1, memory2};
+  }
+}
