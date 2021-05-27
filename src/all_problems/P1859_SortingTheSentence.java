95c1508214825737dfaf538e13a55546d7dcb7e3
YC-S <yuanchenshi@gmail.com>
2021-05-27 11:08:33 -0400

    P1859_SortingTheSentence

---
+++ src/all_problems/P1859_SortingTheSentence.java
@@ -0,0 +1,11 @@
+package all_problems;
+
+public class P1859_SortingTheSentence {
+  public String sortSentence(String s) {
+    String[] ans = new String[s.split(" ").length];
+    for (String st : s.split(" ")) {
+      ans[st.charAt(st.length() - 1) - '1'] = st.substring(0, st.length() - 1);
+    }
+    return String.join(" ", ans);
+  }
+}
