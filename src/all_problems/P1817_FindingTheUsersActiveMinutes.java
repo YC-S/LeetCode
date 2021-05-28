61e31dc4b8914c8a1533da4b8fadfdeb4167cbff
YC-S <yuanchenshi@gmail.com>
2021-05-28 13:14:30 -0400

    P1817_FindingTheUsersActiveMinutes

---
+++ src/all_problems/P1817_FindingTheUsersActiveMinutes.java
@@ -0,0 +1,21 @@
+package all_problems;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class P1817_FindingTheUsersActiveMinutes {
+  public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+    int[] res = new int[k];
+    Map<Integer, Set<Integer>> map = new HashMap<>();
+    for (int[] l : logs) {
+      map.putIfAbsent(l[0], new HashSet<>());
+      map.get(l[0]).add(l[1]);
+    }
+    for (int key : map.keySet()) {
+      res[map.get(key).size() - 1]++;
+    }
+    return res;
+  }
+}
