aca686f8489b2ab5bc80c515be602cea1bc77db0
YC-S <yuanchenshi@gmail.com>
2021-05-28 13:06:29 -0400

    P1838_FrequencyOfTheMostFrequentElement

---
+++ src/all_problems/P1838_FrequencyOfTheMostFrequentElement.java
@@ -0,0 +1,20 @@
+package all_problems;
+
+import java.util.Arrays;
+
+public class P1838_FrequencyOfTheMostFrequentElement {
+  public int maxFrequency(int[] A, int k) {
+    int res = 1, i = 0, j;
+    long sum = 0;
+    Arrays.sort(A);
+    for (j = 0; j < A.length; ++j) {
+      sum += A[j];
+      while (sum + k < (long) A[j] * (j - i + 1)) {
+        sum -= A[i];
+        i += 1;
+      }
+      res = Math.max(res, j - i + 1);
+    }
+    return res;
+  }
+}
