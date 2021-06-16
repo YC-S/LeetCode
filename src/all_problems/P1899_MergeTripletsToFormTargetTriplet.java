33b4761d21b3f5b424a2826239ac5a74bceb9ae8
YC-S <yuanchenshi@gmail.com>
2021-06-16 18:39:27 -0400

    P1899_MergeTripletsToFormTargetTriplet

--- src/all_problems/P1899_MergeTripletsToFormTargetTriplet.java
+++ src/all_problems/P1899_MergeTripletsToFormTargetTriplet.java
@@ -3,13 +3,13 @@ package all_problems;
 public class P1899_MergeTripletsToFormTargetTriplet {
   public boolean mergeTriplets(int[][] triplets, int[] target) {
     int[] hopefulTriplet = new int[3];
-    for (int i = 0; i < triplets.length; i++) {
-      if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2]) {
+    for (int[] triplet : triplets) {
+      if (triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) {
         continue;
       } else {
-        hopefulTriplet[0] = Math.max(hopefulTriplet[0], triplets[i][0]);
-        hopefulTriplet[1] = Math.max(hopefulTriplet[1], triplets[i][1]);
-        hopefulTriplet[2] = Math.max(hopefulTriplet[2], triplets[i][2]);
+        hopefulTriplet[0] = Math.max(hopefulTriplet[0], triplet[0]);
+        hopefulTriplet[1] = Math.max(hopefulTriplet[1], triplet[1]);
+        hopefulTriplet[2] = Math.max(hopefulTriplet[2], triplet[2]);
       }
     }
 
