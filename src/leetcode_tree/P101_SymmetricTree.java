8802eaffe8a4e46a5ac0b2f34f57454c1b4d7fb3
YC-S <yuanchenshi@gmail.com>
2021-07-15 11:41:16 -0700

    101 symmetric tree

---
+++ src/leetcode_tree/P101_SymmetricTree.java
@@ -0,0 +1,34 @@
+package leetcode_tree;
+
+public class P101_SymmetricTree {
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    return helper(root.left, root.right);
+  }
+
+  private boolean helper(TreeNode left, TreeNode right) {
+    if (left == null && right == null) return true;
+    if (left == null || right == null) return false;
+    return left.val == right.val
+        && helper(left.left, right.right)
+        && helper(left.right, right.left);
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
