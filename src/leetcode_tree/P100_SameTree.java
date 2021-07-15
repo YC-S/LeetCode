65713d4f8fda78f27ea3bfb56dada58b7b667f88
YC-S <yuanchenshi@gmail.com>
2021-07-15 11:56:25 -0700

    100 Same Tree

---
+++ src/leetcode_tree/P100_SameTree.java
@@ -0,0 +1,27 @@
+package leetcode_tree;
+
+public class P100_SameTree {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (p == null || q == null) return false;
+    return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
