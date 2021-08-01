3af9707310f70e1dee21a56a2ac38577078a1d1a
YC-S <yuanchenshi@gmail.com>
2021-07-31 17:16:08 -0700

    example 1 comment

---
+++ src/book/precise_java/chapter4/Example1.java
@@ -0,0 +1,13 @@
+package book.precise_java.chapter4;
+
+public class Example1 {
+  public static void main(String[] args) {
+    // This is a one-line comment;
+    /*
+    This is a delimited comment,
+    extending over several lines
+     */
+    int /*This delimited comment extends over part of a line */ x = 117;
+    System.out.println(x);
+  }
+}
