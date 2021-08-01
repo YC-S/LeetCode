200ca87556b9427719f4b8af79f01ca8b6430041
YC-S <yuanchenshi@gmail.com>
2021-07-31 17:10:06 -0700

    puzzle 6 multicast

---
+++ src/book/javapuzzles/chapter2ExpressivePuzzlers/Puzzle6Multicast.java
@@ -0,0 +1,7 @@
+package book.javapuzzles.chapter2ExpressivePuzzlers;
+
+public class Puzzle6Multicast {
+  public static void main(String[] args) {
+    System.out.println((int) (char) (byte) -1);
+  }
+}
